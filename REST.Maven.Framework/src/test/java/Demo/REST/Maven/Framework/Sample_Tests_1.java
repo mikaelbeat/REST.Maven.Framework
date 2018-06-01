e36f8ec99cc687c7ba7e7100b0db4582c990bb4c
Ryynänen,Petri <petri.ryynanen@accenture.com>
2018-06-01 15:51:54 +0300

    Update

---
+++ REST.Maven.Framework/src/test/java/Demo/REST/Maven/Framework/Sample_Tests_1.java
@@ -0,0 +1,17 @@
+package Demo.REST.Maven.Framework;
+
+import org.junit.Test;
+
+public class Sample_Tests_1 {
+	
+	@Test
+	public void First_Test() {
+		System.out.println("My first test.");
+	}
+	
+	@Test
+	public void Second_Test() {
+		System.out.println("My second test.");
+	}
+
+}
---
+++ REST.Maven.Framework/src/test/java/Demo/REST/Maven/Framework/Sample_Tests_2.java
@@ -0,0 +1,17 @@
+package Demo.REST.Maven.Framework;
+
+import org.junit.Test;
+
+public class Sample_Tests_2 {
+	
+	@Test
+	public void Third_Test() {
+		System.out.println("My third test.");
+	}
+	
+	@Test
+	public void Fourth_Test() {
+		System.out.println("My fourth test.");
+	}
+
+}

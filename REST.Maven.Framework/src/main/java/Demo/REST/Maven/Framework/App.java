8626cdf0c620570f58b6e5573e6abda4ad45a234
Ryynänen,Petri <petri.ryynanen@accenture.com>
2018-06-01 14:05:09 +0300

    Initial commit

---
+++ REST.Maven.Framework/src/main/java/Demo/REST/Maven/Framework/App.java
@@ -0,0 +1,13 @@
+package Demo.REST.Maven.Framework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

342d94cf5a4e190c0a1336d46a1dc72cf22d2c61
Ryynänen,Petri <petri.ryynanen@accenture.com>
2018-06-04 11:58:16 +0300

    Update

--- REST.Maven.Framework/src/test/java/Demo/REST/Maven/Framework/Sample_1_Test.java
+++ REST.Maven.Framework/src/test/java/Demo/REST/Maven/Framework/Sample_1_Test.java
@@ -1,19 +1,34 @@
 package Demo.REST.Maven.Framework;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.testng.annotations.Test;
 
 public class Sample_1_Test {
 	
+	private static Logger log = LogManager.getLogger(Sample_1_Test.class.getName());
+	
 	@Test
 	public void First_Test() {
-		System.out.println("My first test.");
+		log.debug("My first test.");
 	}
 	
 	@Test
 	public void Second_Test() {
-		System.out.println("My second test.");
+		log.info("My second test.");
+		log.fatal("Fataali errori");
 	}
 	
-	// Video 12,37
+	@Test
+	public  void Third_Test() {
+		int i = 10;
+		try {
+			int j = i / 0;;
+		} catch (Exception e) {
+			log.error(e);
+		}
+	}
+	
+	
 
 }
